d91a5a55845c697c94840d4b1cb1c6314c36a940
Francis Ferri <francisferrir@gmail.com>
2021-06-15 10:51:29 -0500

    Agregando peticion de backup aws

--- crumb.sh
+++ crumb.sh
@@ -1,6 +1,6 @@
 API_JENKINS=1176390b4c04daa16337656e9c0cd9f041
 curl -u jenkins:$API_JENKINS -X POST http://jenkins.local:8080/job/ENV/build?delay=0sec
 
-curl -u jenkins:$API_JENKINS -X POST http://35.232.33.230:8080/job/db-ansible-users/buildWithParameters?EDAD=23
+curl -u jenkins:$API_JENKINS -X POST http://35.232.33.230:8080/job/db-ansible-users/buildWithParameters?EDAD=25
 
-# curl -u "jenkins:1234" -H "$crumb" -X POST  http://jenkins.local:8080/job/backup-db-to-aws/buildWithParameters?DB_HOST=db_host&DB_NAME=testdb&BUCKET_NAME=jenkins-udemy
+curl -u jenkins:$API_JENKINS -X POST  http://jenkins.local:8080/job/backup-db-to-aws/buildWithParameters?DB_HOST=db_host&DB_NAME=testdb&BUCKET_NAME=jenkins-udemy-francis
