19080b038afc28ebd596b4079229559b140da147
Francis Ferri <francisferrir@gmail.com>
2021-06-15 10:24:30 -0500

    Alterando comando para API KEY

--- crumb.sh
+++ crumb.sh
@@ -1,4 +1,3 @@
-crumb=$(curl -u "jenkins:1234" -s 'http://jenkins.local:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-curl -u "jenkins:1234" -H "$crumb" -X POST http://jenkins.local:8080/job/ENV/build?delay=0sec
+API_JENKINS=1176390b4c04daa16337656e9c0cd9f041
+curl -u jenkins:$API_JENKINS -X POST http://jenkins.local:8080/job/ENV/build?delay=0sec
 # curl -u "jenkins:1234" -H "$crumb" -X POST  http://jenkins.local:8080/job/backup-db-to-aws/buildWithParameters?DB_HOST=db_host&DB_NAME=testdb&BUCKET_NAME=jenkins-udemy
-
